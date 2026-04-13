86872c526d56feccd6addb832e859979586a88d1
nurzhiq <veatube74@gmail.com>
2026-04-13 19:30:07 +0200

    module 4 solution

---
+++ SpeakGoodBye.js
@@ -0,0 +1,12 @@
+(function (window) {
+  var byeSpeaker = {};
+
+  var speakWord = "Goodbye";
+
+  byeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  };
+
+  window.byeSpeaker = byeSpeaker;
+
+})(window);
